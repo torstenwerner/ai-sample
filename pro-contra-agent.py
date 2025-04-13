a358fc30d4843c738b287c1a0832b13d1e6d51fd
Torsten Werner <mail.twerner@gmail.com>
2025-04-13 18:30:59 +0200

    async

--- pro-contra-agent.py
+++ pro-contra-agent.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from google.adk.agents.loop_agent import LoopAgent
 from google.adk.agents.llm_agent import LlmAgent
 from google.genai import types
@@ -16,7 +18,8 @@ SESSION_ID = "session_01"
 GEMINI_MODEL = "gemini-2.0-flash"
 
 # --- State Keys ---
-STATE_STATEMENT = "SQL databases are better than NoSQL databases."
+# STATE_STATEMENT = "SQL databases are better than NoSQL databases."
+STATE_STATEMENT = "The Python programming language is better suited for writing AI code than JavaScript"
 STATE_PRO = "supporting_arguments"
 STATE_CONTRA = "opposing_arguments"
 
@@ -27,11 +30,11 @@ pro_agent = LlmAgent(
     You are a participant in a discussion.
     Check the session state for key '{STATE_PRO}'.
     If '{STATE_PRO}' does NOT exist or is empty, write a few (1-2 sentence) arguments that are in favour of the statement in state key '{STATE_STATEMENT}'.
-    If the session key '{STATE_PRO}' *already exists* and session key '{STATE_CONTRA}', improve and extend '{STATE_PRO}' to contradict the contra arguments in '{STATE_CONTRA}'."
+    If the session key '{STATE_PRO}' *already exists*, update it to contradict the opposing arguments in '{STATE_CONTRA}'."
     Output *only* the story or the exact pass-through message.
     """,
     description="Writes the supporting arguments.",
-    output_key=STATE_PRO # Saves output to state
+    output_key=STATE_PRO
 )
 
 # Critic Agent (LlmAgent)
@@ -43,11 +46,11 @@ critic_agent = LlmAgent(
     You are against the statement in state key '{STATE_STATEMENT}' and against the supporting arguments in state key '{STATE_PRO}'.
     Check the session state for key '{STATE_CONTRA}'.
     If '{STATE_CONTRA}' does NOT exist or is empty, write a few (1-2 sentence) arguments that are against of the statement in state key '{STATE_STATEMENT}' and against the supporting arguments in state key '{STATE_PRO}'.
-    If the session key '{STATE_CONTRA}' *already exists*, improve and extend '{STATE_CONTRA}' to contradict the statement and the supporting arguments"
+    If the session key '{STATE_CONTRA}' *already exists*, update '{STATE_CONTRA}' to oppose the supporting arguments in state key '{STATE_PRO}'.
     Output *only* the story or the exact pass-through message.
     """,
     description="Writes the opposing arguments.",
-    output_key=STATE_CONTRA # Saves critique to state
+    output_key=STATE_CONTRA
 )
 
 # Create the LoopAgent
@@ -61,14 +64,14 @@ session = session_service.create_session(app_name=APP_NAME, user_id=USER_ID, ses
 runner = Runner(agent=loop_agent, app_name=APP_NAME, session_service=session_service)
 
 # Agent Interaction
-def call_agent(query):
+async def call_agent(query):
     content = types.Content(role='user', parts=[types.Part(text=query)])
-    events = runner.run(user_id=USER_ID, session_id=SESSION_ID, new_message=content)
+    events = runner.run_async(user_id=USER_ID, session_id=SESSION_ID, new_message=content)
 
-    for event in events:
+    async for event in events:
         if event.is_final_response():
             final_response = event.content.parts[0].text.strip()
             print(f"{event.author}: {final_response}", end="\n\n")
 
 print(f"Statement: {STATE_STATEMENT}", end="\n\n")
-call_agent("execute")
+asyncio.run(call_agent("execute"))
