f445763e466af8eece6496f6f18d73f1d88c3611
Torsten Werner <mail.twerner@gmail.com>
2025-04-18 21:07:35 +0200

    add summarize_agent.py

---
+++ summarize_agent.py
@@ -0,0 +1,96 @@
+import asyncio
+import mimetypes
+
+from dotenv import load_dotenv
+from google.adk.agents.llm_agent import LlmAgent
+from google.adk.artifacts.in_memory_artifact_service import InMemoryArtifactService
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.adk.tools import ToolContext, FunctionTool
+from google.genai import types
+
+load_dotenv()
+
+
+async def fetch_file_by_name(filename: str, tool_context: ToolContext):
+    """Fetches a file by filename and adds it to the user prompt.
+        Args:
+        filename (str): The name of the file to fetch from the filesystem
+    """
+    with open(filename, "rb") as f:
+        file_content = f.read()
+    mime_type = mimetypes.guess_type(filename)[0] or "application/octet-stream"
+    file_artifact = types.Part.from_bytes(
+        data=file_content,
+        mime_type=mime_type,
+    )
+    tool_context.user_content.parts.append(file_artifact)
+
+
+async def get_agent_async():
+    """Creates an ADK Agent equipped with tools from the MCP Server."""
+    root_agent = LlmAgent(
+        model='gemini-2.5-flash-preview-04-17',
+        name='filesystem_assistant',
+        instruction="""
+        You are summarizing the content of a single file that is specified by the user.
+        1. Fetch the file by its filename using the tool fetch_file_by_name that will add the file content to the user prompt.
+        2. Summarize the file content.
+        """,
+        tools=[FunctionTool(fetch_file_by_name)]
+    )
+    return root_agent
+
+
+async def run_agent(runner, session):
+    while True:
+        query = input("You: ")
+        if not query.strip():
+            print("Exiting interactive session.")
+            break
+
+        content = types.Content(role='user', parts=[types.Part(text=query)])
+
+        events_async = runner.run_async(
+            session_id=session.id, user_id=session.user_id, new_message=content
+        )
+
+        async for event in events_async:
+            if event.is_final_response():
+                final_response = event.content.parts[0].text.strip()
+                print(f"final_response: {final_response}")
+            else:
+                for part in event.content.parts:
+                    if part.text:
+                        print(f"text: {part.text.strip()}")
+                    elif part.function_call:
+                        print(f"function_call: name={part.function_call.name} args={part.function_call.args}")
+                    elif part.function_response:
+                        print(f"function_response: name={part.function_response.name}")
+                    else:
+                        print(part)
+
+
+async def async_main():
+    session_service = InMemorySessionService()
+    # Artifact service might not be needed for this example
+    artifacts_service = InMemoryArtifactService()
+
+    session = session_service.create_session(
+        state={}, app_name='mcp_filesystem_app', user_id='user_fs'
+    )
+
+    root_agent = await get_agent_async()
+
+    runner = Runner(
+        app_name='mcp_filesystem_app',
+        agent=root_agent,
+        artifact_service=artifacts_service,  # Optional
+        session_service=session_service,
+    )
+
+    await run_agent(runner, session)
+
+
+if __name__ == '__main__':
+    asyncio.run(async_main())
