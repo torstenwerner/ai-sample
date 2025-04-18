1a95959e9d24c7257abf1db42aae284ce68bf59c
Torsten Werner <mail.twerner@gmail.com>
2025-04-18 08:25:44 +0200

    test gemma3

---
+++ gemma3.py
@@ -0,0 +1,40 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+
+
+def generate():
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    # model = "gemma-3-27b-it"
+    model = "gemma-3-12b-it"
+    # model = "gemma-3-4b-it"
+    # model = "gemma-3-1b-it"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""How to answer the greeting: \"how do you do?\""""),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        response_mime_type="text/plain",
+    )
+
+    for chunk in client.models.generate_content_stream(
+            model=model,
+            contents=contents,
+            config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+
+if __name__ == "__main__":
+    generate()
